2ac9586502e4e4d9366c8f85abdc643002cd8e56
majestronik <frknkuru16@gmail.com>
2024-02-18 17:08:36 -0500

    Creating MicroService Car Auction Website

---
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,29 @@
+﻿using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace AuctionService.RequestHelpers;
+
+public class MappingProfiles : Profile
+{
+  public MappingProfiles()
+  {
+    CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+    CreateMap<Item, AuctionDto>();
+    CreateMap<CreateAuctionDto, Auction>()
+      .ForMember(d => d.Item, o => o.MapFrom(s => CreateItemFromDto(s)));
+  }
+  private Item CreateItemFromDto(CreateAuctionDto dto)
+  {
+    return new Item
+    {
+      Make = dto.Make,
+      Model = dto.Model,
+      Year = dto.Year,
+      Color = dto.Color,
+      Mileage = dto.Mileage,
+      ImageUrl = dto.ImageUrl,
+    };
+  }
+
+}
