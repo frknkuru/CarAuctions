129dc798653dd5941e938e911b64758ac9acd9d1
majestronik <frknkuru16@gmail.com>
2024-03-01 21:33:21 -0500

    🔥 refactor(AuctionSvcHttpClient.cs): remove unused code and simplify GetItemsForSearchDb method
    The commented out code was not being used and was unnecessary. Removed it to improve code readability and maintainability. The GetItemsForSearchDb method now simply makes a GET request to the AuctionServiceUrl to retrieve a list of items for the search database.

--- src/SearchService/Services/AuctionSvcHttpClient.cs
+++ src/SearchService/Services/AuctionSvcHttpClient.cs
@@ -15,12 +15,12 @@ public class AuctionSvcHttpClient
 
   public async Task<List<Item>> GetItemsForSearchDb()
   {
-    var lastUpdated = await DB.Find<Item, string>()
-      .Sort(x => x.Descending(x => x.UpdatedAt))
-      .Project(x => x.UpdatedAt.ToString())
-      .ExecuteFirstAsync();
+    // var lastUpdated = await DB.Find<Item, string>()
+    //   .Sort(x => x.Descending(x => x.UpdatedAt))
+    //   .Project(x => x.UpdatedAt.ToString())
+    //   .ExecuteFirstAsync();
     return await _httpClient.GetFromJsonAsync<List<Item>>(_config["AuctionServiceUrl"]
-        + "/api/auctions?date=" + lastUpdated);
+        + "/api/auctions");
   }
 
 }
