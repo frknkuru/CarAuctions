16f18ba447529987800ec441fcd2c551697b694b
majestronik <frknkuru16@gmail.com>
2024-03-23 23:02:45 -0400

    fix(authActions.ts): remove unnecessary console.log statement feat(route.ts): add jwt and session callbacks to handle token and session data feat(Logo.tsx): add logic to redirect to home page and reset state on logo click feat(Navbar.tsx): pass user prop to UserActions component feat(Search.tsx): add logic to redirect to home page on search feat(UserActions.tsx): add UserActions component to display user actions and logout functionality feat(next-auth.d.ts): declare custom types for user session and profile data

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -7,11 +7,12 @@ import UserActions from './UserActions'
 
 export default async function Navbar() {
   const user = await getCurrentUser()
+
   return (
     <header className='sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md'>
       <Logo />
       <Search />
-      {user ? (<UserActions />) : (<LoginButton />)}
+      {user ? (<UserActions user={user} />) : (<LoginButton />)}
     </header>
   )
 }
